5e11d943f75876c9321619c351fc019765ae55d8
AdityaKrSha <adityakrsha52@gmail.com>
2025-09-01 15:43:26 +0530

    Assignment completed

---
+++ task_1.py
@@ -0,0 +1,18 @@
+a=input('Enter the first Number : ')
+b=input('Enter the second Number : ')
+a=int(a)
+b=int(b)
+c=a+b
+c=int(c)
+d=a-b
+d=int(d)
+e=a*b
+e=int(e)
+f=a/b
+f=float(f)
+print('Addition: ', c)
+print('Substraction: ', d)
+print('Multiplication: ', e)
+print('Division: ', f)
+
+
