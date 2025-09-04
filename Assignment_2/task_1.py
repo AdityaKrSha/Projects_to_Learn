c8eb5ba63f137c520fb27997157e25339bb25b22
AdityaKrSha <adityakrsha52@gmail.com>
2025-09-04 18:51:29 +0530

    Assignment completed

---
+++ Assignment_2/task_1.py
@@ -0,0 +1,7 @@
+a=int(input('Enter the number: '))
+c=a%2
+
+if c==0:
+    print( a, 'number is even')
+else:
+    print(a, 'number is odd')
---
+++ if_else.py
@@ -0,0 +1,8 @@
+a=int(input('Enter the number: '))
+
+if a>100:
+    print('Sorry, your number is too high.')
+elif a<100:
+    print('Sorry, your number is too low.')
+else:
+    print('Thank You')
